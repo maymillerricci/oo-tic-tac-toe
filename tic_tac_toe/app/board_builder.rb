56a28de1708ad3cabb9a75270f8583990bd6d0ff
May Miller-Ricci <maymillerricci@gmail.com>
2020-05-06 13:39:04 -0700

    Use .map instead of .each and then no longer need board attr_accessor

--- tic_tac_toe/app/board_builder.rb
+++ tic_tac_toe/app/board_builder.rb
@@ -3,19 +3,11 @@ require_relative "row_builder"
 class BoardBuilder
   BOARD_SIZE = 3
 
-  attr_accessor :board
-
-  def initialize
-    @board = ""
-  end
-
   def run
-    (0..BOARD_SIZE * 2 + 1).each do |x|
-      (0..BOARD_SIZE).each do |y|
-        self.board += RowBuilder.new(x, y).run
+    (0..BOARD_SIZE * 2 + 1).map do |x|
+      (0..BOARD_SIZE).map do |y|
+        RowBuilder.new(x, y).run
       end
-    end
-
-    board
+    end.join
   end
 end
