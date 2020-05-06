fc268da2212378d12225f3ca389b4b339a01c8a4
May Miller-Ricci <maymillerricci@gmail.com>
2020-05-06 11:50:31 -0700

    Add failing test for board with coordinates

--- spec/integration/game_runner_spec.rb
+++ spec/integration/game_runner_spec.rb
@@ -1,17 +1,18 @@
 require_relative "../../tic_tac_toe/app/game_runner"
 
 describe "Game runner" do
-  it "Outputs a blank game board" do
+  it "Outputs a blank game board with coordinates" do
     mock_stdin = StringIO.new
     mock_stdout = StringIO.new
     expected_board_string = <<-BOARD
-     __ __ __
-    |  |  |  |
-    |__|__|__|
-    |  |  |  |
-    |__|__|__|
-    |  |  |  |
-    |__|__|__|
+       1  2  3
+       __ __ __
+    A |  |  |  |
+      |__|__|__|
+    B |  |  |  |
+      |__|__|__|
+    C |  |  |  |
+      |__|__|__|
     BOARD
 
     GameRunner.new(mock_stdin, mock_stdout).run
